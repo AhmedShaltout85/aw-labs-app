8203f1423962766652c52372e05d1459f4ef5d92
AhmedShaltout85 <eng.abozaher@gmail.com>
2024-03-18 10:39:11 +0200

    create aw-labs-app project

---
+++ src/main/java/com/a08r/awlabsapp/AwLabsAppApplication.java
@@ -0,0 +1,15 @@
+package com.a08r.awlabsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwLabsAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AwLabsAppApplication.class, args);
+	}
+
+
+}
